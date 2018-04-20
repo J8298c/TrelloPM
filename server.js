1f90aadab33e5001984c1381a0929864d2d0a6a9
j8298c <juliocmojica@gmail.com>
2018-04-19 21:08:05 -0400

    nodemon and configurations

--- server.js
+++ server.js
@@ -1,8 +1,13 @@
 const express = require('express');
 const bodyParser = require('body-parser');
 const config = require('./server/config');
+const mongoose = require('mongoose');
 
 const app = express();
+
+mongoose.connect(config.DBURI, () => {
+  console.log('connected to mongo successfully');
+});
 
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
--- server/config.js
+++ server/config.js
@@ -1,6 +1,15 @@
 const env = process.env.NODE_ENV;
 
-module.exports = {
-	DB_URI: env === prod ? MONGO_URI : "mongodb://localhost:27017/myproject",
-	PORT: env === prod ? process.env.PORT : 3001
-};
+switch (env) {
+  case 'production':
+    module.exports = {
+      PORT: process.env.PORT,
+      DBURI: process.env.DBURI
+    };
+    break;
+  default:
+    module.exports = {
+      PORT: 3001,
+      DBURI: 'mongodb://localhost:27017/projectpm'
+    };
+}
