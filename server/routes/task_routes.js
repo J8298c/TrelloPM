644906fa162b31ffdf994f4edfa56a3e8b7a1afb
j8298c <juliocmojica@gmail.com>
2018-04-19 21:46:32 -0400

    task.post

--- server/routes/task_routes.js
+++ server/routes/task_routes.js
@@ -4,4 +4,34 @@ module.exports = app => {
   app.get('/task', (req, res) => {
     res.send('I am working');
   });
+
+  app.post('/task', (req, res) => {
+    const {
+      createdOn,
+      createdBy,
+      dueOn,
+      assignedTo,
+      taskBody,
+      status
+    } = req.body;
+
+    let task = new Task({
+      createdOn,
+      createdBy,
+      dueOn,
+      assignedTo,
+      taskBody,
+      status
+    });
+
+    task.save((err, task) => {
+      if (err) errorHandler(err);
+
+      res.json({ status: 200, task });
+    });
+  });
 };
+
+function errorHandler(err) {
+  console.log(err);
+}
