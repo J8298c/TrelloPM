7750a146dace92d27ad20d9301f868c1c7e27d87
j8298c <juliocmojica@gmail.com>
2018-04-20 22:46:37 -0400

    testing

--- server/config.js
+++ server/config.js
@@ -8,6 +8,15 @@ switch (env) {
       SECRET: process.env.SECRET
     };
     break;
+
+  case 'test':
+    module.exports = {
+      PORT: 3002,
+      DBURI: 'mongodb://localhost:27017/projectpmtest',
+      SECRET: 'Ilovelittledebbie'
+    };
+    break;
+
   default:
     module.exports = {
       PORT: 3001,
