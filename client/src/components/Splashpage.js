292eee7f3fa9f452ddcb58301caa595182e303cd
j8298c <juliocmojica@gmail.com>
2018-04-22 00:43:26 -0400

    themenig splashpage

--- client/src/components/Splashpage.js
+++ client/src/components/Splashpage.js
@@ -1,10 +1,24 @@
 import React from 'react';
+import { Grid, Form, Container } from 'semantic-ui-react';
 
 const Splashpage = () => (
-  <div>
-    <div>
-      <h1 className="app-title">SplashPage</h1>
-    </div>
-  </div>
+  <Grid>
+    <Grid.Row className="splash-top" />
+    <Grid.Row className="splash-header">
+      <Grid.Column>
+        <h1 className="app-title">AppTitle</h1>
+      </Grid.Column>
+    </Grid.Row>
+    <Grid.Row className="splash-body">
+      <Grid.Column width={3} reversed>
+        <Container>
+          <Form>
+            <Form.Input placeholder="Enter email" />
+            <Form.Input placeholder="Enter Password" />
+          </Form>
+        </Container>
+      </Grid.Column>
+    </Grid.Row>
+  </Grid>
 );
 export default Splashpage;
