f1df9e784eb2e049b01459a66eb2caad07e8b125
j8298c <juliocmojica@gmail.com>
2018-04-22 15:51:52 -0400

    signing user up

--- client/src/components/splashpage/Splashpage.js
+++ client/src/components/splashpage/Splashpage.js
@@ -1,11 +1,35 @@
-import React, { Component } from 'react';
-import { Grid, Form, Container, Image } from 'semantic-ui-react';
-import AuthForm from './AuthForm';
+import React, { Component } from "react";
+import { Grid, Form, Container, Image } from "semantic-ui-react";
+import AuthForm from "./AuthForm";
 
 class Splashpage extends Component {
   state = {};
 
   onAcctSignUp() {
+    if (
+      this.state.signUpPassword &&
+      this.state.signUpPassword === this.state.passwordConfirm
+    ) {
+      const { signUpEmail, signUpPassword } = this.state;
+      fetch("/register", {
+        method: "POST",
+        headers: {
+          "content-type": "application/json"
+        },
+        body: JSON.stringify({ email: signUpEmail, password: signUpPassword })
+      })
+        .then(res => res.json())
+        .then(json => {
+          if (json.success) {
+            //dispatch fetch a user and push user to welcome screen
+          }
+        })
+        .catch(err => console.log(err));
+    } else {
+      this.setState({
+        message: "Confirmation password doesnt match password provided"
+      });
+    }
     console.log(this.state);
   }
 
@@ -15,7 +39,7 @@ class Splashpage extends Component {
 
   render() {
     return (
-      <Grid style={{ height: '100%' }}>
+      <Grid style={{ height: "100%" }}>
         <Grid.Row className="splash-top" />
         <Grid.Row className="splash-header">
           <Grid.Column>
@@ -25,7 +49,7 @@ class Splashpage extends Component {
         <Grid.Row className="splash-body">
           <Grid.Column width={12}>
             <div className="splash-img-container">
-              <Image fluid src={require('../../assests/landpageimg.png')} />
+              <Image fluid src={require("../../assests/landpageimg.png")} />
             </div>
           </Grid.Column>
           <Grid.Column width={3} reversed>
