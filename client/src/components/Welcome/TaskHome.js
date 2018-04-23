53d8643111ba232598f802a37fd8c14a8cee2e8e
j8298c <juliocmojica@gmail.com>
2018-04-22 22:25:11 -0400

    task home grid title styling

--- client/src/components/Welcome/TaskHome.js
+++ client/src/components/Welcome/TaskHome.js
@@ -1,9 +1,20 @@
 import React, { Component } from 'react';
 import { connect } from 'react-redux';
-
+import { Container, Grid } from 'semantic-ui-react';
 class TaskHome extends Component {
   render() {
-    return <div>taskhome</div>;
+    return (
+      <Container>
+        <Grid celled>
+          <Grid.Row columns={4}>
+            <Grid.Column className="title-cell">This Month</Grid.Column>
+            <Grid.Column className="title-cell">Owner</Grid.Column>
+            <Grid.Column className="title-cell">Task</Grid.Column>
+            <Grid.Column className="title-cell">Due on</Grid.Column>
+          </Grid.Row>
+        </Grid>
+      </Container>
+    );
   }
 }
 
